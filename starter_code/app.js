ea46d709a39283db0c4099fe6d64eb5f54090515
Josue Valencia <josue.valencia@domo.ec>
2019-03-26 19:09:44 +0100

    navbar ready

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
+
 const path    = require('path');
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +12,20 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/random-beers', (req, res, next) => {
+  res.render('randomBeer');
+});
+
+app.get('/beer', (req, res, next) => {
+  res.render('beer');
+});
 
 
 app.listen(3000);
